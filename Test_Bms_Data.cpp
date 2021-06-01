c984a09f2587fee01e78fcc42006a785cb08b069
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-02 03:19:39 +0530

    Update Test_Bms_Data.cpp

--- Test_Bms_Data.cpp
+++ Test_Bms_Data.cpp
@@ -1,2 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
+#include "dataBmsTx.h"
+
+TEST_CASE("Check_1") 
+{
+ REQUIRE(dataBmsMain (void) == OK_STATUS);
+}
