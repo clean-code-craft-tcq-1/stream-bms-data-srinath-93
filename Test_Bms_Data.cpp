3822917d1b626371bb2f75e2e0b6dad9a7f99417
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-02 04:08:54 +0530

    Update Test_Bms_Data.cpp

--- Test_Bms_Data.cpp
+++ Test_Bms_Data.cpp
@@ -2,7 +2,12 @@
 #include "test/catch.hpp"
 #include "Tx/dataBmsTx.h"
 
-TEST_CASE("Check_1") 
+TEST_CASE(" Test case to check if data is read and sender sends data ") 
 {
  REQUIRE(dataBmsMain() == OK_STATUS);
+}
+
+TEST_CASE(" Test case to check if data read is happening and returns ok status ") 
+{
+ REQUIRE(readBmsData() == OK_STATUS);
 }
