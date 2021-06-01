fe40a4d871e2088a77952e2eed617aa2b3c994a6
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-02 01:36:33 +0530

    Update dataBmsTx.h

--- Tx/dataBmsTx.h
+++ Tx/dataBmsTx.h
@@ -1,1 +1,24 @@
+#ifndef DATABMSTX_H_
+#define DATABMSTX_H_
 
+/* Macro Declaration */
+#define ARRINDEXSIZE 50
+
+/* Enum declarations */
+typedef enum
+{
+  OK_STATUS,
+  ERROR_STATUS
+}retBmsStatus_en;
+
+/* Structure declarations */
+typedef struct
+{
+  float batteryTempearature[ARRINDEXSIZE];
+  float batterySoc[ARRINDEXSIZE];
+}batteryReading_st;
+
+/* Function declaration */
+retBmsStatus_en dataBmsMain();
+retBmsStatus_en readBmsData();
+retBmsStatus_en txBmsData();
