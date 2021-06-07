779b4391444b5f980c17df73e7e993fa565d103a
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-07 12:10:53 +0530

    Update Test_Bms_Data.cpp

--- Test_Bms_Data.cpp
+++ Test_Bms_Data.cpp
@@ -4,12 +4,12 @@
 
 TEST_CASE(" Test case to check if data is read and sender sends data ") 
 {
- int runTimeIpNum_u32 = 50;
+ int runTimeIpNum_u32 = 49;
  REQUIRE(dataBmsMain(runTimeIpNum_u32) == OK_STATUS);
 }
 
 TEST_CASE(" Test case to check if data read is happening and returns ok status ") 
 {
- int runTimeIpNum_u32 = 50;
+ int runTimeIpNum_u32 = 49;
  REQUIRE(readBmsData(runTimeIpNum_u32) == OK_STATUS);
 }
