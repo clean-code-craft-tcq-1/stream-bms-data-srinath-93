b078bb021c135cf328c55aed07bc220f670de4d1
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-07 22:52:06 +0530

    Update dataBmsTx.c

--- Tx/dataBmsTx.c
+++ Tx/dataBmsTx.c
@@ -46,7 +46,7 @@ retBmsStatus_en readBmsData(int runTimeIpNum)
   long f_size;
   FILE * fileToBeRead= fopen("./Tx/dataBms.txt","r");
   long f_size = ftell(fileToBeRead);
-  printf("file size is %d \n",f_size);
+  printf("file size is %ld \n",f_size);
   if (fileToBeRead)
   {
     for(;fscanf(fileToBeRead, "%f %f\n", &dataTemperature,&dataSoc)!=EOF;cntrLoop++)
