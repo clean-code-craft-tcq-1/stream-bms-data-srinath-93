bd4d3ea91d481d241da15d80dde83a1eaa040cd9
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-06-17 15:27:39 +0530

    Update simpleMovingAverage.py

--- Receiver/simpleMovingAverage.py
+++ Receiver/simpleMovingAverage.py
@@ -1,1 +1,25 @@
+def calculateAvg(bms_data):
+    temperature_values = []
+    soc_values = []
+    final_display_list = {}
 
+    for i in range(-5, 0, 1):
+        temperature_values.append(float(bms_data[i][0][1]))
+        soc_values.append(float(bms_data[i][1][1]))
+
+    soc_values.sort()
+    temperature_values.sort()
+    final_display_list['temperature'] = temperature_values
+    final_display_list['soc'] = soc_values
+    displayOutput(final_display_list)
+    
+def displayOutput(bms_data):
+    print('Max Temperature:', bms_data['temperature'][-1])
+    print('Min Temperature:', bms_data['temperature'][0])
+    print('Average Temperature:', average(bms_data['temperature']))
+    print('Max SOC:', bms_data['soc'][-1])
+    print('Min SOC:', bms_data['soc'][0])
+    print('Average SOC:', average(bms_data['soc']))
+    
+def average(list):
+    return round(sum(list) / len(list), 2)
