1124ed7bc3293c08cbe47f39c374e6cb06765e0a
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-06-19 16:22:12 +0530

    Update inputProcessor.py

--- Receiver/inputProcessor.py
+++ Receiver/inputProcessor.py
@@ -1,21 +1,41 @@
 import simpleMovingAverage
 
 def getBMSData():
-    final_bms_data = []
-    while(True):
-        bms_details = input()
-        bms_details = formatInputStream(bms_details)
-        final_bms_data.append(bms_details)
-        if len(final_bms_data) >= 5:
-            simpleMovingAverage.calculateAvg(final_bms_data)
+    bms_details = input()
+    list_temp,list_soc = formatInputStream(bms_details)
+    if len(list_temp) >= 5 and len(list_soc) >=5:
+        simpleMovingAverage.calculateAvg(list_temp,list_soc)
+    
+def dataValidation(bms_details):
+    length = len(bms_details)
+    if length <5:
+        print('please input more than 5 values')
+        exit
+    return True
+
+def parameterSeperation(bms_details):
+    dict_query = {}
+    list_temp = []
+    list_soc = []
+    print(bms_details)
+    for i in bms_details:
+        try:
+            key, val = i.split("=", 1)
+            dict_query.update({key:float(val)})
+            if key == 'Temperature':
+                list_temp.append(dict_query['Temperature'])
+            else:
+                list_soc.append(dict_query['StateOfCharge'])
+        except ValueError:
+            continue
+    return list_temp,list_soc
+
 def formatInputStream(bms_details):
-    bms_details = bms_details.replace('Battery Temperature[.+','temperature')
-    bms_details = bms_details.replace('Battery StateOfCharge[.+','soc')
-    bms_details = bms_details.replace('->', '=')
-    bms_details = bms_details.replace(' ', '')
-    bms_details = bms_details.split('\t')
-    bms_details[0] = bms_details[0].split('=')
-    bms_details[1] = bms_details[1].split('=')
-    return bms_details
-  
+    bms_details = bms_details.replace('\n',',')
+    bms_details = bms_details.replace(' ' ,'')
+    bms_details = bms_details.split(',')
+    if(dataValidation(bms_details)==True):
+        list_temp,list_soc = parameterSeperation(bms_details)
+        return list_temp,list_soc
+    
 getBMSData()
