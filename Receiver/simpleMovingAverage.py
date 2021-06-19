ab1c30b35160470998fed81a3bebc8b5787837f2
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-06-19 16:23:14 +0530

    Update simpleMovingAverage.py

--- Receiver/simpleMovingAverage.py
+++ Receiver/simpleMovingAverage.py
@@ -1,25 +1,22 @@
-def calculateAvg(bms_data):
-    temperature_values = []
-    soc_values = []
+def calculateAvg(list_temp,list_soc):
     final_display_list = {}
-
-    for i in range(-5, 0, 1):
-        temperature_values.append(float(bms_data[i][0][1]))
-        soc_values.append(float(bms_data[i][1][1]))
-
-    soc_values.sort()
-    temperature_values.sort()
-    final_display_list['temperature'] = temperature_values
-    final_display_list['soc'] = soc_values
+    list_temp.sort()
+    list_soc.sort()
+    final_display_list['temperature'] = list_temp
+    final_display_list['soc'] =list_soc
+    print(final_display_list)
     displayOutput(final_display_list)
-    
 def displayOutput(bms_data):
     print('Max Temperature:', bms_data['temperature'][-1])
     print('Min Temperature:', bms_data['temperature'][0])
-    print('Average Temperature:', average(bms_data['temperature']))
     print('Max SOC:', bms_data['soc'][-1])
     print('Min SOC:', bms_data['soc'][0])
-    print('Average SOC:', average(bms_data['soc']))
+    print('Average of last five values of temperature and soc', averageLastFive(bms_data['temperature'],bms_data['soc']))
     
-def average(list):
-    return round(sum(list) / len(list), 2)
+def averageLastFive(list_temp,list_soc):
+    sum_temp = 0
+    sum_soc = 0
+    for i in range(-1,-6,-1):
+        sum_temp += list_temp[i]
+        sum_soc += list_soc[i]
+        return sum_temp/5,sum_soc/5
