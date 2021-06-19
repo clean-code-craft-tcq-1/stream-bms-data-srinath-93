ccc434b3c066794329d7dae95ce370cc7655035d
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-06-19 17:18:16 +0530

    Update inputProcessor.py

--- Receiver/inputProcessor.py
+++ Receiver/inputProcessor.py
@@ -1,8 +1,9 @@
 import simpleMovingAverage
 import sys
 def getBMSData():
-    while(True):
-        bms_details = input()
+    list_temp = []
+    list_soc = []
+    for i in sys.stdin:
         list_temp,list_soc = formatInputStream(bms_details)
     if len(list_temp) >= 5 and len(list_soc) >=5:
        simpleMovingAverage.calculateAvg(list_temp,list_soc)
